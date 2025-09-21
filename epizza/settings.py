df5dcd377fb86bd79fde86714b3911ec00e30050
dimi1441 <dkamdem26@gmail.com>
2025-09-21 02:14:59 +0200

    test to fix build error during push. on va voir le paquet qui cause l erreur 12

--- epizza/settings.py
+++ epizza/settings.py
@@ -92,9 +92,13 @@ WSGI_APPLICATION = "epizza.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'mydb'),
+        'USER': os.getenv('DB_USER', 'myuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
